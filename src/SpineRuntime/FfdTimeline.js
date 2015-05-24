bce7918f2c9df34b207d91de9d5f38226416face
Simon Ouellet <souellet@tadram.com>
2015-05-23 23:59:37 -0400

    fix remaining issue with mesh

--- src/SpineRuntime/FfdTimeline.js
+++ src/SpineRuntime/FfdTimeline.js
@@ -33,11 +33,12 @@ spine.FfdTimeline.prototype = {
         var vertexCount = frameVertices[0].length;
 
         var vertices = slot.attachmentVertices;
-        if (vertices.length != vertexCount) alpha = 1;
+        if (vertices.length != vertexCount) alpha = 1;// Don't mix from uninitialized slot vertices.
         
-        if(vertices.length < vertexCount){
-            for(var k = vertices.length; k < vertexCount; k++) vertices.push(0);
-        } 
+        //re init all vertices to 0 
+        vertices = []
+        for(var k = 0; k < vertexCount; k++) vertices.push(0);
+                
 
         if (time >= frames[frames.length - 1])
         { // Time is after last frame.
