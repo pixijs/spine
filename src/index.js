517e471bbe7a702bd4731a214e61b632c071d1d1
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2016-09-20 18:55:15 +0300

    adding core

--- src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @namespace PIXI.spine
- */
-module.exports = PIXI.spine = { // "PIXI.spine" assignment is here for people/plugins who use plugin both through require and as a plugin.
-    Spine:          require('./Spine'),
-    SpineRuntime:   require('./SpineRuntime'),
-    loaders:        require('./loaders')
-};
--- src/loaders/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    atlasParser: require('./atlasParser'),
-    Loader: require('./Loader'),
-    syncImageLoaderAdapter: require('./syncImageLoaderAdapter'),
-    imageLoaderAdapter: require('./imageLoaderAdapter')
-};
