9c8a8f504da14135cfecdc83d5ed21430a62e1a1
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2017-06-21 18:53:42 +0300

    dserp module

--- src/core/attachments/ClippingAttachment.ts
+++ src/core/attachments/ClippingAttachment.ts
@@ -28,7 +28,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *****************************************************************************/
 
-module pixi_spine.core {
+namespace pixi_spine.core {
 	export class ClippingAttachment extends VertexAttachment {
 		endSlot: SlotData;
 
