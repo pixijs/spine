5e4312fb813fc5e4b9e64ba51ec9e26308e2826d
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2018-03-17 21:55:45 +0300

    fix for v5

--- src/exporter.ts
+++ src/exporter.ts
@@ -1,3 +1,11 @@
 namespace pixi_spine {
     (PIXI as any).spine = pixi_spine;
+
+    // texture patch for v5
+
+    const TextureProto = PIXI.Texture.prototype as any;
+
+    if (!TextureProto._updateUvs) {
+        TextureProto._updateUvs = TextureProto.updateUvs;
+    }
 }
