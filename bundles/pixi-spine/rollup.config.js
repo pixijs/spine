4f10798cdd9e3ea6b545dd6f1d714936096e4442
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2021-04-12 15:38:22 +0300

    moved all

--- bundles/pixi-spine/rollup.config.js
+++ bundles/pixi-spine/rollup.config.js
@@ -2,7 +2,7 @@ const { main } = require('@pixi-build-tools/rollup-configurator/main');
 
 module.exports = main({
     globals: {
-        '@pixi-spine/base': 'PIXI.spine.base',
+        '@pixi-spine/base': 'PIXI.spine',
         '@pixi-spine/runtime-3.8': 'PIXI.spine',
         '@pixi-spine/loaders-3.8': 'PIXI.spine',
     },
