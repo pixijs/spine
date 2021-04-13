fecb31f435a8bfeb919ad6ba32d66bede527e753
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2021-04-13 14:22:57 +0300

    crude rollup config for bundling pixi-spine umd

--- bundles/pixi-spine/rollup.config.js
+++ bundles/pixi-spine/rollup.config.js
@@ -1,9 +1,24 @@
 const { main } = require('@pixi-build-tools/rollup-configurator/main');
 
-module.exports = main({
+const results = main({
     globals: {
         '@pixi-spine/base': 'PIXI.spine',
         '@pixi-spine/runtime-3.8': 'PIXI.spine',
-        '@pixi-spine/loaders-3.8': 'PIXI.spine',
+        '@pixi-spine/loader-3.8': 'PIXI.spine',
     },
 });
+
+// TODO: get sorted deps of all our @pixi-spine deps
+
+const umdDeps = ['@pixi/app', '@pixi/constants', '@pixi/core', '@pixi/display', '@pixi/graphics',
+    '@pixi/loaders', '@pixi/math', '@pixi/mesh-extras', '@pixi/sprite', '@pixi/utils'];
+
+results.forEach((entry) => {
+    if (entry.output.format === 'umd') {
+        entry.external = entry.external.filter((moduleName) => {
+            return moduleName.indexOf('@pixi-spine') !== 0;
+        }).concat(umdDeps);
+    }
+})
+
+module.exports = results;
