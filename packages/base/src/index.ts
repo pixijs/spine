f9038323ea9833246ce3858482f0dfa52d1c59a7
Liam Faulkner <liam.faulkner@tombola.com>
2021-08-17 10:49:29 +0100

    fix: exporting new constraint types

--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -2,6 +2,7 @@
 export * from './core/AttachmentType';
 export * from './core/BinaryInput';
 export * from './core/ISkeleton';
+export * from './core/IConstraint';
 export * from './core/TextureAtlas';
 export * from './core/TextureRegion';
 export * from './core/Utils';
