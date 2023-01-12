d219025c5c939a2923003136e1dfa27c986bb6a6
Milton <miltoncandelero@gmail.com>
2023-01-12 15:55:03 -0300

    all 3.8

--- bundles/all-3.8/src/index.ts
+++ bundles/all-3.8/src/index.ts
@@ -1,5 +1,5 @@
-import {SpineParser} from '@pixi-spine/loader-3.8';
-export {SpineParser};
+import { SpineParser } from '@pixi-spine/loader-3.8';
+export { SpineParser };
 export * from '@pixi-spine/runtime-3.8';
 export * from '@pixi-spine/base';
 
