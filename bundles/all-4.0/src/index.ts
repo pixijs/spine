76cfaeef646ee1726cd560f8c525d7e3365cccd3
Milton <miltoncandelero@gmail.com>
2023-01-12 16:00:33 -0300

    all 4.0

--- bundles/all-4.0/src/index.ts
+++ bundles/all-4.0/src/index.ts
@@ -1,5 +1,5 @@
-import {SpineParser} from '@pixi-spine/loader-4.0';
-export {SpineParser};
+import { SpineParser } from '@pixi-spine/loader-4.0';
+export { SpineParser };
 export * from '@pixi-spine/runtime-4.0';
 export * from '@pixi-spine/base';
 
