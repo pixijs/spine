73ed726e455116387eb7caa40030ad418b05fd1a
Milton <miltoncandelero@gmail.com>
2023-01-16 12:03:24 -0300

    all 4.0

--- bundles/all-4.0/src/index.ts
+++ bundles/all-4.0/src/index.ts
@@ -1,6 +1,4 @@
-import { SpineParser } from '@pixi-spine/loader-4.0';
-export { SpineParser };
+import '@pixi-spine/loader-4.0'; // Side effect install the loader
+// eslint-disable-next-line @typescript-eslint/no-duplicate-imports
 export * from '@pixi-spine/runtime-4.0';
 export * from '@pixi-spine/base';
-
-SpineParser.registerLoaderPlugin();
